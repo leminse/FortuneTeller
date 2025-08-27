9a0537bf4f968d607f32959dc3e240bd7de848bc
minseo <minseo@DESKTOP-NL04HGM>
2025-08-27 13:52:47 +0900

    #2 중복으로 내역 불러오기 창이 뜨는 것을 수정

--- Form1.cs
+++ Form1.cs
@@ -24,8 +24,16 @@ namespace FortuneTeller
 
         private void 내역불러오기ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            FormPstory form = new FormPstory();
-            form.Show();
+            FormPstory form = Application.OpenForms["FormPstory"] as FormPstory;
+            if (form != null)
+            {
+                form.Activate();
+            }
+            else
+            {
+                form = new FormPstory();
+                form.Show();
+            }
         }
 
         private void 끝내기ToolStripMenuItem_Click(object sender, EventArgs e)
