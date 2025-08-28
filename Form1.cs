aec61b4f081550b07f7853e80160dbe6bc5d50f6
minseo <minseo@DESKTOP-NL04HGM>
2025-08-28 13:57:18 +0900

    #4 사주 보기 버튼 클릭, 사주 보기 및 내역 저장 구현
    UI 수정 및 운세 계산 기능 추가

--- Form1.cs
+++ Form1.cs
@@ -85,5 +85,34 @@ namespace FortuneTeller
             form.ShowDialog();
         }
 
+        private void btnResult_click(object sender, EventArgs e)
+        {
+            string birthday = tbBirthday.Text;
+            string birthour = tbBirthour.Text;
+            string result = GetFortune();
+            string saju = result.Split('|')[0];
+            string message = result.Split('|')[1];
+            tbResult.Text = $"{birthday} {birthour} {Environment.NewLine}"
+                + $"{saju} {Environment.NewLine}"
+                + $"{message}";
+            SaveHistory($"{birthday} {birthour} | {result}");
+        }
+
+        private void SaveHistory(string history)
+        {
+            try
+            {
+                string filename = "history.csv";
+                File.AppendAllText(filename, history + Environment.NewLine);
+            }
+            catch(UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"권한 없음 오류 발생 \n {ex.Message}", "권한 오류");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"알 수 없는 오류 발생 \n {ex.Message}", "알 수 없는 오류");
+            }
+        }
     }
 }
